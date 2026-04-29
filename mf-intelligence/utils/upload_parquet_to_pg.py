648f2f6516c62eaceef1c765eddca0a090bcceb4
doralalam <doralalam.babu@gmail.com>
2026-04-29 18:08:30 +0530

    latest changes in pgsql

--- mf-intelligence/utils/upload_parquet_to_pg.py
+++ mf-intelligence/utils/upload_parquet_to_pg.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 OUTPUT_FILE = "/Users/dorababulalam/GitHub/Projects/mf-intelligence/data/master_dataset/all_amc/all_amc_clean.parquet"
 
@@ -7,6 +7,10 @@ df = pd.read_parquet(OUTPUT_FILE)
 
 engine = create_engine("postgresql+psycopg2://postgres:1234@localhost:5433/mf_intelligence")
 
-df.to_sql("amc_master", engine, if_exists="replace", index=False)
+with engine.connect() as conn:
+    conn.execute(text("TRUNCATE TABLE amc_master CASCADE;"))
+    conn.commit()
+
+df.to_sql("amc_master", engine, if_exists="append", index=False)
 
 print("Data uploaded to PostgreSQL successfully!!!")
